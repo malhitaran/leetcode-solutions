78ae9ea86f7dfae869cb80462e82760b0934af74
Taranm1 <MalhiTaran182005@gmail.com>
2026-02-08 17:35:24 +0000

    Update Maximum Number of Vowels in a Substring of Given Length (Medium) [Python3]

--- solutions/1456-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ solutions/1456-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,6 +1,6 @@
 # Maximum Number of Vowels in a Substring of Given Length (Medium)
 # https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
-# Accepted 2026-02-08 — Python3, runtime 65 ms, memory 20.1 MB
+# Accepted 2026-02-08 — Python3, runtime 63 ms, memory 19.7 MB
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
 
