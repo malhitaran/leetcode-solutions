3fad051097d7b6012220312667633152b9339903
Taranm1 <MalhiTaran182005@gmail.com>
2026-04-26 12:26:25 +0100

    Update Merge Strings Alternately (Easy) [Python3]

--- solutions/1768-merge-strings-alternately/merge-strings-alternately.py
+++ solutions/1768-merge-strings-alternately/merge-strings-alternately.py
@@ -1,18 +1,18 @@
 # Merge Strings Alternately (Easy)
 # https://leetcode.com/problems/merge-strings-alternately/
-# Accepted 2026-04-26 — Python3, runtime 49 ms, memory 19.1 MB
+# Accepted 2026-04-26 — Python3, runtime 42 ms, memory 19.3 MB
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
 
         
-        newStr=[]
+        newStr=""
         for i in range(max(len(word1), len(word2))):
             if i>len(word1)-1:
-                newStr.append(word2[i])
+                newStr+=word2[i]
             elif i>len(word2)-1:
-                newStr.append(word1[i])
+                newStr+=word1[i]
             else:
-                newStr.append(word1[i])
-                newStr.append(word2[i])
+                newStr+=word1[i]
+                newStr+=word2[i]
 
-        return ''.join(newStr)
+        return newStr
