93066ff9309cee21350a5029cf5b0f26e88fa33b
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-14 18:25:09 +0100

    Update Path Sum III (Medium) [Python3]

--- solutions/0437-path-sum-iii/path-sum-iii.py
+++ solutions/0437-path-sum-iii/path-sum-iii.py
@@ -1,53 +1,44 @@
 # Path Sum III (Medium)
 # https://leetcode.com/problems/path-sum-iii/
-# Accepted 2026-05-14 — Python3, runtime 465 ms, memory 19.8 MB
+# Accepted 2026-05-14 — Python3, runtime 3 ms, memory 20.6 MB
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        
-        '''
+        self.paths=0
+        self.pathSums= defaultdict(int)
+        self.pathSums[0]=1
 
-        2 functions 
-
-        1 for counting from every node
-        1 to give every node
-        '''
-
-
-        def current_sum(node, curSum):
-
-
-            count=0
-            if not node:
-                return 0
-            
-            curSum+=node.val
-
-            if curSum==targetSum:
-                count+=1
-            
-            count+=current_sum(node.left, curSum)
-            count+=current_sum(node.right, curSum)
-
-            return count
-
-        def every_node(node):
+        def DFS(node, currSum):
 
             if not node:
-                return 0
+                return
+            currSum+=node.val
+            self.paths+=self.pathSums[currSum-targetSum]
+            self.pathSums[currSum]+=1
+            
+            if node.left:
+                DFS(node.left, currSum)
+            if node.right:
+                DFS(node.right, currSum)
 
-            total=0
+            self.pathSums[currSum]-=1
 
-            total+=current_sum(node, 0)
+        DFS(root, 0)
+        return self.paths
+        '''
 
-            total+=every_node(node.left)
-            total+=every_node(node.right)
+        next solution is path sums
 
-            return total
+        so we store a running count of the total
+        we can subtract the target and see if a previous path sum equates to the difference
 
-        return every_node(root)
+        then we know we cna remove that path and get the solution
+        so we increase our path sum
+
+        '''
