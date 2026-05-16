f92b3fb2cbe3caa4a489fe41644ddaf5d4e4dbff
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-16 08:09:09 +0100

    Update Can Place Flowers (Easy) [Python3]

--- solutions/0605-can-place-flowers/can-place-flowers.py
+++ solutions/0605-can-place-flowers/can-place-flowers.py
@@ -1,22 +1,49 @@
 # Can Place Flowers (Easy)
 # https://leetcode.com/problems/can-place-flowers/
-# Accepted 2026-04-26 — Python3, runtime 8 ms, memory 19.6 MB
+# Accepted 2026-05-16 — Python3, runtime 7 ms, memory 19.5 MB
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+
+        '''
+
+        [1,0,0,0,1]
+
+        n=1
+
+
+        2 cases
+
+
+        its a 1 or its a 0
+
+        for a 1 we just skip
+
+        if its a zero we can cheeck previous and after and reduce n
+
+
+        '''
         
-        count = 0
-        m = len(flowerbed)
 
-        for i in range(m):
-            if flowerbed[i] == 0:
-                left_empty = (i == 0 or flowerbed[i - 1] == 0)
-                right_empty = (i == m - 1 or flowerbed[i + 1] == 0)
 
-                if left_empty and right_empty:
-                    flowerbed[i] = 1
-                    count += 1
+        for i in range(len(flowerbed)):
 
-                    if count >= n:
-                        return True
+            if flowerbed[i]==1:
+                continue
+            else:
 
-        return count >= n
+                #start case
+                if i==0 and len(flowerbed)>1:
+                    if flowerbed[i+1]==0:
+                        flowerbed[i]=1
+                        n-=1
+                #end case
+                elif i ==len(flowerbed)-1:
+                    if flowerbed[i-1]==0:
+                        flowerbed[i]=1
+                        n-=1
+                elif flowerbed[i-1]==0 and flowerbed[i+1]==0 and flowerbed[i]==0:
+                    flowerbed[i]=1
+                    n-=1
+        
+        return n<=0
