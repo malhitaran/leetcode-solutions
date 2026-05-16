deabc96f90da8e2ad97f20207ba649010e2f36e8
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-16 07:44:58 +0100

    Update Greatest Common Divisor of Strings (Easy) [Python3]

--- solutions/1071-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ solutions/1071-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,11 +1,23 @@
 # Greatest Common Divisor of Strings (Easy)
 # https://leetcode.com/problems/greatest-common-divisor-of-strings/
-# Accepted 2025-09-09 — Python3, runtime 0 ms, memory 17.8 MB
-from math import gcd
-
+# Accepted 2026-05-16 — Python3, runtime 0 ms, memory 19.3 MB
+import math
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1 + str2 != str2 + str1:
-            return ""
-        gcd_len = gcd(len(str1), len(str2))
-        return str1[:gcd_len]
+        
+        '''
+        way 1
+        brute force solution 
+
+        '''
+
+        shorter = str1 if len(str1)<len(str2) else str2
+
+        for length in range(len(shorter), 0, -1):
+            candidate=shorter[:length]
+
+            if (len(str1) % len(candidate) ==0) and (len(str2) % len(candidate)==0):
+                if (candidate * (len(str1) // length)==str1) and (candidate *(len(str2) // length)==str2):
+                    return candidate
+
+        return ""
