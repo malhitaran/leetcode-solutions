a0ffc5503f4449fa00605b3124e59180332dea45
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-16 08:17:52 +0100

    Update Can Place Flowers (Easy) [Python3]

--- solutions/0605-can-place-flowers/can-place-flowers.py
+++ solutions/0605-can-place-flowers/can-place-flowers.py
@@ -1,49 +1,24 @@
 # Can Place Flowers (Easy)
 # https://leetcode.com/problems/can-place-flowers/
-# Accepted 2026-05-16 — Python3, runtime 7 ms, memory 19.5 MB
+# Accepted 2026-05-16 — Python3, runtime 15 ms, memory 19.5 MB
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
 
 
         '''
 
-        [1,0,0,0,1]
-
-        n=1
-
-
-        2 cases
-
-
-        its a 1 or its a 0
-
-        for a 1 we just skip
-
-        if its a zero we can cheeck previous and after and reduce n
-
+        cleaner solution
 
         '''
-        
-
-
         for i in range(len(flowerbed)):
+            left=(i==0)or(flowerbed[i-1]==0)
+            right=(i==len(flowerbed)-1)or(flowerbed[i+1]==0)
 
-            if flowerbed[i]==1:
-                continue
-            else:
+            if flowerbed[i]==0 and left and right:
+                flowerbed[i]=1
+                n-=1
 
-                #start case
-                if i==0 and len(flowerbed)>1:
-                    if flowerbed[i+1]==0:
-                        flowerbed[i]=1
-                        n-=1
-                #end case
-                elif i ==len(flowerbed)-1:
-                    if flowerbed[i-1]==0:
-                        flowerbed[i]=1
-                        n-=1
-                elif flowerbed[i-1]==0 and flowerbed[i+1]==0 and flowerbed[i]==0:
-                    flowerbed[i]=1
-                    n-=1
-        
+                if n==0:
+                    return True
+
         return n<=0
