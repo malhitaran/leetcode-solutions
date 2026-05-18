cd43112d752415b4855ef45f24a30291016ccc59
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-18 10:31:48 +0100

    Update Maximum Average Subarray I (Easy) [Python3]

--- solutions/0643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ solutions/0643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,17 +1,14 @@
 # Maximum Average Subarray I (Easy)
 # https://leetcode.com/problems/maximum-average-subarray-i/
-# Accepted 2026-02-08 — Python3, runtime 57 ms, memory 29.6 MB
+# Accepted 2026-05-18 — Python3, runtime 55 ms, memory 29.1 MB
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-
-        #[1,12,-5,-6,50,3]
         
-        window_sum=sum(nums[:k])
-        window_max=window_sum
+        window_sum = sum(nums[:k])
+        best_sum = window_sum
 
         for i in range(k, len(nums)):
-            window_sum+=nums[i]-nums[i-k]
-            if window_sum>window_max:
-                window_max=window_sum
-            
-        return window_max/k
+            window_sum += nums[i] - nums[i - k]
+            best_sum = max(best_sum, window_sum)
+
+        return best_sum / k
