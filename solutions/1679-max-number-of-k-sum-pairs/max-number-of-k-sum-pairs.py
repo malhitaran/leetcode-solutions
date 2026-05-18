3f3b71cb44eca3371c6082463392f9099543ce8b
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-18 10:01:10 +0100

    Update Max Number of K-Sum Pairs (Medium) [Python3]

--- solutions/1679-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ solutions/1679-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,22 +1,35 @@
 # Max Number of K-Sum Pairs (Medium)
 # https://leetcode.com/problems/max-number-of-k-sum-pairs/
-# Accepted 2026-02-07 — Python3, runtime 457 ms, memory 31.5 MB
+# Accepted 2026-05-18 — Python3, runtime 456 ms, memory 31.4 MB
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        operations=0
-        lhs=0
-        rhs=len(nums)-1
+        
+        '''
+
+        [1,2,3,4]
+
+        3 cases
+        front and back equal both move in
+        front and back is less move the less back/foward
+        opposite of above
+        '''
+
+        [1,3,3,3,4]
+
+
+        front=0
+        back=len(nums)-1
+        ops=0
+
         nums.sort()
-
-        while lhs<rhs:
-            sum=nums[lhs]+nums[rhs]
-            if sum==k:
-                operations+=1
-                rhs-=1
-                lhs+=1
-            elif sum<k:
-                lhs+=1
-            else:
-                rhs-=1
-
-        return operations
+        while front<back:
+            if nums[front]+nums[back]==k:
+                ops+=1
+                back-=1
+                front+=1
+            elif nums[front]+nums[back]<k:
+                front+=1
+            elif nums[front]+nums[back]>k:
+                back-=1
+            
+        return ops
