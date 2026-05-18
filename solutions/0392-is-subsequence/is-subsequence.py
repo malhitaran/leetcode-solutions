8b3a36e217e47a8eeaf083e8ceb398bdf28ccf05
Taranm1 <MalhiTaran182005@gmail.com>
2026-05-18 09:28:58 +0100

    Update Is Subsequence (Easy) [Python3]

--- solutions/0392-is-subsequence/is-subsequence.py
+++ solutions/0392-is-subsequence/is-subsequence.py
@@ -1,18 +1,15 @@
 # Is Subsequence (Easy)
 # https://leetcode.com/problems/is-subsequence/
-# Accepted 2026-02-07 — Python3, runtime 0 ms, memory 19.3 MB
+# Accepted 2026-05-18 — Python3, runtime 0 ms, memory 19.2 MB
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i=0
-        j=0
-        while j<len(t) and i<len(s):
-            if t[j]!=s[i]:
-                j+=1
-            else:
-                i+=1
-                j+=1
 
-        if i==len(s):
-            return True
-        else:
-            return False
+
+
+        i = 0
+
+        for char in t:
+            if i < len(s) and char == s[i]:
+                i += 1
+
+        return i == len(s)
