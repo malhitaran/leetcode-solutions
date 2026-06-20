3be22ad51d60995798199d6f798aecb4469cef54
Taranm1 <MalhiTaran182005@gmail.com>
2026-06-20 16:54:11 +0100

    Update Valid Anagram (Easy) [Python3]

--- solutions/0242-valid-anagram/valid-anagram.py
+++ solutions/0242-valid-anagram/valid-anagram.py
@@ -1,20 +1,14 @@
 # Valid Anagram (Easy)
 # https://leetcode.com/problems/valid-anagram/
-# Accepted 2026-06-12 — Python3, runtime 7 ms, memory 19.9 MB
+# Accepted 2026-06-20 — Python3, runtime 3 ms, memory 19.3 MB
 from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
         if len(s)!=len(t):
             return False
-        
-        
-        counts1=list(s)
-
-        counts1=Counter(counts1)
-
-        counts2=list(t)
-
-        counts2=Counter(counts2)
-        
-        return counts2==counts1
+    
+        x=Counter(s)
+        t=Counter(t)
+        return x==t
