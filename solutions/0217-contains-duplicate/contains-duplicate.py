26680ff0498fbacf7d66a15bdfbbb426e2e6ee81
Taranm1 <MalhiTaran182005@gmail.com>
2026-06-20 16:47:19 +0100

    Update Contains Duplicate (Easy) [Python3]

--- solutions/0217-contains-duplicate/contains-duplicate.py
+++ solutions/0217-contains-duplicate/contains-duplicate.py
@@ -1,16 +1,12 @@
 # Contains Duplicate (Easy)
 # https://leetcode.com/problems/contains-duplicate/
-# Accepted 2026-06-12 — Python3, runtime 12 ms, memory 32.5 MB
+# Accepted 2026-06-20 — Python3, runtime 11 ms, memory 32.2 MB
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        newSet=set()
-
+        output=set()
         for num in nums:
-
-            if num in newSet:
+            if num in output:
                 return True
-
-            else: newSet.add(num)
-
+            output.add(num)
         return False
