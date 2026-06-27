b0743d3a5dfc694fb1a25fef8faa6166453925b4
Taranm1 <MalhiTaran182005@gmail.com>
2026-06-27 09:37:12 +0100

    Update Binary Search (Easy) [Python3]

--- solutions/0704-binary-search/binary-search.py
+++ solutions/0704-binary-search/binary-search.py
@@ -1,27 +1,19 @@
 # Binary Search (Easy)
 # https://leetcode.com/problems/binary-search/
-# Accepted 2026-06-27 — Python3, runtime 0 ms, memory 20.4 MB
+# Accepted 2026-06-27 — Python3, runtime 0 ms, memory 20.5 MB
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
         start=0
         end=len(nums)-1
-
-        midpoint=((end-start)//2)+start
+                
         while start<=end:
-            
-            print(start)
-            print(end)
-            print(midpoint)
-            
+            midpoint=((end-start)//2)+start
             if nums[midpoint]==target:
                 return midpoint
-
             elif nums[midpoint]>target:
                 end=midpoint-1
-                midpoint=((end-start)//2)+start
-
             else:
                 start=midpoint+1
-                midpoint=((end-start)//2)+start
+
         return -1
