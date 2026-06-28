7070823f22da935af26de05c76e720e8964fcd5d
Taranm1 <MalhiTaran182005@gmail.com>
2026-06-28 10:42:59 +0100

    Update Linked List Cycle (Easy) [Python3]

--- solutions/0141-linked-list-cycle/linked-list-cycle.py
+++ solutions/0141-linked-list-cycle/linked-list-cycle.py
@@ -1,6 +1,6 @@
 # Linked List Cycle (Easy)
 # https://leetcode.com/problems/linked-list-cycle/
-# Accepted 2026-06-28 — Python3, runtime 65 ms, memory 22.8 MB
+# Accepted 2026-06-28 — Python3, runtime 51 ms, memory 22.4 MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -10,15 +10,15 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
-        ourS=set()
-        curr=head
+        slow=head
+        fast=head
 
-        while curr:
-            if curr in ourS:
+        while fast and fast.next:
+
+            slow=slow.next
+            fast=fast.next.next
+
+            if slow==fast:
                 return True
-            else:
-                ourS.add(curr)
-
-            curr=curr.next
 
         return False
