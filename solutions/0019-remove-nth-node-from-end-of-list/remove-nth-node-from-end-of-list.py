914565ae9fc30b90b2649d37e86efaeb57a0eefe
Taranm1 <MalhiTaran182005@gmail.com>
2026-06-28 14:29:44 +0100

    Update Remove Nth Node From End of List (Medium) [Python3]

--- solutions/0019-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ solutions/0019-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,6 +1,6 @@
 # Remove Nth Node From End of List (Medium)
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-# Accepted 2026-06-14 — Python3, runtime 2 ms, memory 19.3 MB
+# Accepted 2026-06-28 — Python3, runtime 0 ms, memory 19.3 MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -8,41 +8,23 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-            
-            curr = head
-            prev=curr
-            counter=0
 
+            curr=head
+            count=0
             while curr:
-                counter += 1
+                count+=1
                 curr=curr.next
-                
+            print(count)
+        
+            dummy=ListNode()
+            dummy.next=head
+            curr=dummy
             
-            diff=counter-n
-            
-            curr = head 
-            for i in range(diff-1):
-                curr = curr.next
-            
-            print(curr, counter)
-
-            if diff == 0:
+            if count-n==0:
                 return head.next
-            nxt = curr.next.next
-            curr.next = nxt
-
-            return head
-
-
-
-
-            '''
-            while i!=diff:
-                prevP=currP
-                currP=currP.next
-                i+=1
-                
-            prevP.next=currP.next
-
-            return head
-            '''
+            
+            for i in range(count-n):
+                curr=curr.next
+            
+            curr.next=curr.next.next
+            return dummy.next
