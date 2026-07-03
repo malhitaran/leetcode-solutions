afc99879148a0087cbcca9a194c2ae6d44796b3b
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-03 15:17:57 +0100

    Update Longest Substring Without Repeating Characters (Medium) [Python3]

--- solutions/0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ solutions/0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,38 +1,34 @@
 # Longest Substring Without Repeating Characters (Medium)
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
-# Accepted 2026-06-27 — Python3, runtime 44 ms, memory 19.3 MB
+# Accepted 2026-07-03 — Python3, runtime 19 ms, memory 19.3 MB
+
 from collections import defaultdict
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
 
+        '''
 
+
+        '''
         
-        '''
-        "pwwkew"
 
-        w:2
+        seen=defaultdict(int)
 
-        '''
-
-
-        ourD=defaultdict()
         l=0
         r=0
         best=0
-        while r<len(s):
-            
-            letter=s[r]
-            if letter not in ourD:
-                ourD[letter]=1
-                print(ourD)
-            else:
-                ourD[letter]+=1
-                while ourD[letter]>1:
-                    ourD[s[l]]-=1
-                    
-                    l+=1
 
+        while r<len(s):
+
+            seen[s[r]]+=1
+
+            while seen[s[r]]>1:
+                seen[s[l]]-=1
+                l+=1
+            
+            best=max(best, r-l+1)
             r+=1
-            best=max(best, r-l)
+
+        
         return best
