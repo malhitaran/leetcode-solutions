9a27c60e74b6d104bcf041b5a9d564395a4bd0be
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-04 08:16:10 +0100

    Update Search in Rotated Sorted Array (Medium) [Python3]

--- solutions/0033-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ solutions/0033-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,31 +1,43 @@
 # Search in Rotated Sorted Array (Medium)
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
-# Accepted 2026-06-27 — Python3, runtime 0 ms, memory 19.4 MB
+# Accepted 2026-07-04 — Python3, runtime 0 ms, memory 19.4 MB
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        '''
 
         '''
+        binary search
 
-        lo=0
-        hi=len(nums)-1
+        5
 
-        while lo<=hi:
-            mid=(lo+hi)//2
+        [5,6,7,0,1,2,4]
 
-            if nums[mid]==target:
+        '''
+        
+
+        l,r=0, len(nums)-1
+
+        while l<=r:
+
+            mid=(l+r)//2
+
+            if target==nums[mid]:
                 return mid
 
-            #left side
-            if nums[mid]>=nums[lo]:
-                if target<nums[lo] or target>nums[mid]:
-                    lo=mid+1
+            #if were in the left
+            if nums[mid]>=nums[l]:
+
+                if target>nums[mid] or target<nums[l]:
+                    l=mid+1
                 else:
-                    hi=mid-1
-            #right side
+                    r=mid-1
+
+
+
+
+            #if were in the right
             else:
-                if target>nums[hi] or target<nums[mid]:
-                    hi=mid-1
+                if target<nums[mid] or target>nums[r]:
+                    r=mid-1
                 else:
-                    lo=mid+1
+                    l=mid+1
         return -1
