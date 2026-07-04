e753aa538e337c42a699658d8d79e0ad18aefb19
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-04 11:33:26 +0100

    Update Top K Frequent Elements (Medium) [Python3]

--- solutions/0347-top-k-frequent-elements/top-k-frequent-elements.py
+++ solutions/0347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,23 +1,43 @@
 # Top K Frequent Elements (Medium)
 # https://leetcode.com/problems/top-k-frequent-elements/
-# Accepted 2026-07-04 — Python3, runtime 7 ms, memory 22.9 MB
+# Accepted 2026-07-04 — Python3, runtime 10 ms, memory 24.7 MB
 from collections import Counter
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         
+        '''
+
+        bucket sort
+
+        freq where its a list of a list
 
 
-        numFrequency=Counter(nums)
+        [[]] and each index represents the count
 
-        heap=[]
+        and at each index we have a list of numbers
 
-        for num, count in numFrequency.items():
+        and then we go backwards
 
-            heapq.heappush(heap, (count, num))
+        '''
 
-            if len(heap)>k:
-                heapq.heappop(heap)
 
-        return [num for count, num in heap]
+        valueFrequency=Counter(nums)
+
+        frequency=[[] for i in range(len(nums)+1)]
+        
+        res=[]
+        
+        for num, count in valueFrequency.items():
+
+            frequency[count].append(num)
+
+       
+        for i in range(len(nums), 0, -1):
+           
+            for num in frequency[i]:
+
+                res.append(num)
+                if len(res)==k:
+                    return res
