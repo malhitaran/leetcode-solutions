ee5c54172c281b9c8b7044cac408f8e671b10aa0
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-10 10:40:21 +0100

    Update Valid Parentheses (Easy) [Python3]

--- solutions/0020-valid-parentheses/valid-parentheses.py
+++ solutions/0020-valid-parentheses/valid-parentheses.py
@@ -1,6 +1,6 @@
 # Valid Parentheses (Easy)
 # https://leetcode.com/problems/valid-parentheses/
-# Accepted 2026-07-03 — Python3, runtime 0 ms, memory 19.2 MB
+# Accepted 2026-07-10 — Python3, runtime 0 ms, memory 19.4 MB
 class Solution:
     def isValid(self, s: str) -> bool:
         
@@ -10,27 +10,30 @@ class Solution:
 
         '''
 
-        stack=[]
-
-        bracketMapping={
+        pairOfBrackets={
 
             ')':'(',
             '}':'{',
             ']':'['
+
         }
 
-        for bracket in s:
 
-            if bracket=="(" or bracket=="{" or bracket=="[":
-                stack.append(bracket)
-            else:
+        stack=[]
+
+
+        for char in s:
+
+            if char in pairOfBrackets:
                 if stack:
-                    lastBracket=stack.pop()
-                else:
+                    x=stack.pop()
+                elif not stack:
+                    return False
+                if x!= pairOfBrackets[char]:
                     return False
 
-                if bracketMapping[bracket]!=lastBracket:
-                    return False
+            else:
+                stack.append(char)
 
         if stack: 
             return False
