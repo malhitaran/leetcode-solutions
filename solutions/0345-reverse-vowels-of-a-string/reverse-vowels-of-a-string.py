9e39710cbb4a8ce993e8da522f0397d07ea9984a
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-29 20:16:42 +0100

    Update Reverse Vowels of a String (Easy) [Python3]

--- solutions/0345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ solutions/0345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,33 +1,35 @@
 # Reverse Vowels of a String (Easy)
 # https://leetcode.com/problems/reverse-vowels-of-a-string/
-# Accepted 2026-05-16 — Python3, runtime 23 ms, memory 20.6 MB
+# Accepted 2026-07-29 — Python3, runtime 11 ms, memory 21.3 MB
 class Solution:
     def reverseVowels(self, s: str) -> str:
 
+            '''
 
-        '''
-        "IceCreAm"
+            two pointer solution
 
+            '''
 
-        '''
-        s=list(s)
-        front=0
-        back=len(s)-1
+            vowels={'A', 'a', 'E', 'e','I', 'i','O', 'o','U', 'u'}
 
-        vowels=["A","E","I","O","U","a","e","i","o","u",]
-        while front<back:
+            end=len(s)-1
+            start=0
 
-            if s[front] in vowels and s[back] in vowels:
-                temp=s[front]
-                s[front]=s[back]
-                s[back]=temp
-                front+=1
-                back-=1
-
-            if s[front] not in vowels:
-                front+=1
+            s=list(s)
+            print(s)
             
-            if s[back] not in vowels:
-                back-=1
+            while start<end:
 
-        return ''.join(s)
+                if s[start] in vowels and s[end] in vowels:
+                    temp=s[start]
+                    
+                    s[start]=s[end]
+                    s[end]=temp
+                    start+=1
+                    end-=1
+                if s[start] not in vowels:
+                    start+=1
+                if s[end] not in vowels:
+                    end-=1
+           
+            return ''.join(s)
