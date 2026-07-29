82beb074f0e720605015d89b058b6aa3fd8e6bf2
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-29 17:41:51 +0100

    Update Kids With the Greatest Number of Candies (Easy) [Python3]

--- solutions/1431-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ solutions/1431-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,22 +1,16 @@
 # Kids With the Greatest Number of Candies (Easy)
 # https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
-# Accepted 2026-05-16 — Python3, runtime 0 ms, memory 19.2 MB
+# Accepted 2026-07-29 — Python3, runtime 0 ms, memory 19.3 MB
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
 
-        '''
-        an O(n) solution
-        '''
+        ourMax=max(candies)
 
-        boolArray=[]
-        currMax=max(candies)
-
-        
-        for candy in candies:
-
-            if candy+extraCandies>=currMax:
-                boolArray.append(True)
+        res=[]
+        for number in candies:
+            if extraCandies+number>=ourMax:
+                res.append(True)
             else:
-                boolArray.append(False)
+                res.append(False)
 
-        return boolArray
+        return res
