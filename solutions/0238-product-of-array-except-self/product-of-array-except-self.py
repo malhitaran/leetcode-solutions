7dd941464585f3a9addd5ba2297f744bac66b9ee
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-30 19:21:44 +0100

    Update Product of Array Except Self (Medium) [Python3]

--- solutions/0238-product-of-array-except-self/product-of-array-except-self.py
+++ solutions/0238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,57 +1,39 @@
 # Product of Array Except Self (Medium)
 # https://leetcode.com/problems/product-of-array-except-self/
-# Accepted 2026-07-03 — Python3, runtime 39 ms, memory 27.6 MB
+# Accepted 2026-07-30 — Python3, runtime 26 ms, memory 27.2 MB
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
 
+        #prefix
+        prev=1
+        pref=[]
+        for num in nums:
+            pref.append(prev)
+            prev=prev*num
+
+        print(pref)
+
+
         '''
 
-        [1, 1, 2, 6]
-
-        [24,12,4,1]
 
 
         '''
 
-        #wed have a prefix
-        prefixArray=[0]*len(nums)
-
-        for i, num in enumerate(nums):
-
-            if i==0:
-                count=num
-                prefixArray[i]=1
-            else:
-                prefixArray[i]=count
-                count*=num
-       
 
 
+        #suffix
 
-
-
-        #and a suffix
-        suffixArray=[0]*len(nums)
-
+        prev=1
+        suf=[0]*len(nums)
         for i in range(len(nums)-1, -1, -1):
-
-           
-            if i==len(nums)-1:
-                
-                suffixArray[i]=1
-                count=nums[i]
-                
-            else:
-                suffixArray[i]=count
-                count*=nums[i]
-
-        output=[]
-
-        print(prefixArray)
-        print(suffixArray)
         
-        for i in range(len(prefixArray)):
-            output.append(prefixArray[i]*suffixArray[i])
-       
-        return output
+            suf[i]=prev
+            prev=prev*nums[i]
+
+
+        for i in range(len(nums)):
+            suf[i]=suf[i]*pref[i]
+
+        return suf
