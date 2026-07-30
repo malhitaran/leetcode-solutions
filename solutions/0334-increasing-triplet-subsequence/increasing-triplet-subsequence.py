2c808ac00ce14e1246e3824dc8590ac57a83cb5b
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-30 22:21:52 +0100

    Update Increasing Triplet Subsequence (Medium) [Python3]

--- solutions/0334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ solutions/0334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,32 +1,22 @@
 # Increasing Triplet Subsequence (Medium)
 # https://leetcode.com/problems/increasing-triplet-subsequence/
-# Accepted 2026-05-17 — Python3, runtime 45 ms, memory 38.9 MB
+# Accepted 2026-07-30 — Python3, runtime 471 ms, memory 39.3 MB
 
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
 
+        i=float('infinity')
+        j=float('infinity')
 
-        '''
-
-        keep track of the one before and the one before that
-
-        5 1 6
-
-        i 5
-        j 1
-        '''    
-
-        i=float('inf')
-        j=float('inf')
-
-        for x in range(len(nums)):
-            if nums[x]>i and nums[x]<j:
-                j=nums[x]
-            elif nums[x]>i and nums[x]>j:
+        for num in nums:
+            if num>j and j>i:
                 return True
-            elif nums[x]<i:
-                i=nums[x]
+            if num<i:
+                i=num
+            elif num<j and num>i:
+                j=num
 
-
+            print(i)
+            print(j)
         return False
