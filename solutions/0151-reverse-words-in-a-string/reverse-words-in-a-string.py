fa7fb4290a11a34172f1e530f06fea44dc16f59b
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-30 19:09:27 +0100

    Update Reverse Words in a String (Medium) [Python3]

--- solutions/0151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ solutions/0151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,11 +1,10 @@
 # Reverse Words in a String (Medium)
 # https://leetcode.com/problems/reverse-words-in-a-string/
-# Accepted 2026-05-16 — Python3, runtime 0 ms, memory 19.4 MB
+# Accepted 2026-07-30 — Python3, runtime 3 ms, memory 19.4 MB
 class Solution:
     def reverseWords(self, s: str) -> str:
 
         s=s.split()
-        
         s.reverse()
-
+        
         return ' '.join(s)
