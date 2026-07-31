0ac29b4ab4b04a86923b3f806d0f499feabb2a19
Taranm1 <MalhiTaran182005@gmail.com>
2026-07-31 23:39:43 +0100

    Update Two Sum (Easy) [Python3]

--- solutions/0001-two-sum/two-sum.py
+++ solutions/0001-two-sum/two-sum.py
@@ -1,26 +1,15 @@
 # Two Sum (Easy)
 # https://leetcode.com/problems/two-sum/
-# Accepted 2026-07-03 — Python3, runtime 0 ms, memory 20.3 MB
-from collections import Counter
+# Accepted 2026-07-31 — Python3, runtime 3 ms, memory 20.5 MB
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        
 
-        '''
-        3:0
+        seen=dict()
 
-        '''
-
-
-
-        seen={}
-
-
-        for i, num in enumerate(nums):
-
+        for i,num in enumerate(nums):
             if target-num in seen:
-                return [seen[target-num], i]
-            else:
-
-                seen[num]=i
+                return [i, seen[target-num]]
+            if num in seen:
+                continue
+            seen[num]=i
