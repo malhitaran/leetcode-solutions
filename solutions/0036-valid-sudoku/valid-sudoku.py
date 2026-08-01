480ee5f71fb4e9f52a839d9e60f2f09ee2336138
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-01 17:48:55 +0100

    Update Valid Sudoku (Medium) [Python3]

--- solutions/0036-valid-sudoku/valid-sudoku.py
+++ solutions/0036-valid-sudoku/valid-sudoku.py
@@ -1,71 +1,44 @@
 # Valid Sudoku (Medium)
 # https://leetcode.com/problems/valid-sudoku/
-# Accepted 2026-06-14 — Python3, runtime 3 ms, memory 19.2 MB
+# Accepted 2026-08-01 — Python3, runtime 0 ms, memory 19.2 MB
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        '''
 
-        [["5","3",".",".","7",".",".",".","."]
-        ,["6",".",".","1","9","5",".",".","."]
-        ,[".","9","8",".",".",".",".","6","."]
-        ,["8",".",".",".","6",".",".",".","3"]
-        ,["4",".",".","8",".","3",".",".","1"]
-        ,["7",".",".",".","2",".",".",".","6"]
-        ,[".","6",".",".",".",".","2","8","."]
-        ,[".",".",".","4","1","9",".",".","5"]
-        ,[".",".",".",".","8",".",".","7","9"]]
-
-        given this input, we can validate it across 3 constraints
-
-        firstly values have to be unique on the top row
-
-        maybe create a set each time check if its already in set return false
-
-        do the same for columns
-
-        then we work out the final step
 
         '''
-        #check 1
-        rows=9
-        cols=9
-        ourSet=set()
-        for i in range(rows):
-            ourSet.clear()
-            for j in range(cols):
-                if board[i][j] == ".":
+        -we can do 3 sets
+        -we can do one loop
+        -theres a trick to see which box something is in
+
+        our box set can be a tuple, of box num and num
+
+        '''
+
+        rowSet=set()
+        colSet=set()
+        boxSet=set()
+
+        rows=len(board)
+        cols=len(board[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]==".":
                     continue
-                if board[i][j] in ourSet:
+                #calculate box
+                box_index = (r // 3) * 3 + (c // 3)
+                if (r, board[r][c]) in rowSet:
                     return False
                 else:
-                    ourSet.add(board[i][j]) 
-
-        #check 2
-        for i in range(cols):
-            ourSet.clear()
-            for j in range(rows):
-                if board[j][i] == ".":
-                    continue
-                if board[j][i] in ourSet:
+                    rowSet.add((r, board[r][c]))
+                if (c, board[r][c]) in colSet:
                     return False
                 else:
-                    ourSet.add(board[j][i])
-        
-        #check 3(the 3x3)
-        '''
-        we can put each 9 block into sets
-        this tells us the box its in
-        '''
-        setList = [set() for _ in range(9)]
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j]==".":
-                    continue
-                elif board[i][j] in setList[((i//3) *3 +(j//3))]:
+                    colSet.add((c, board[r][c]))
+                if (box_index, board[r][c]) in boxSet:
                     return False
                 else:
-                    setList[((i//3) *3 +(j//3))].add(board[i][j])
-
-    
+                    boxSet.add((box_index, board[r][c]))
         return True
+
+                #now we need to add to there sets
