24cac5010413aa25102b64cf7194404d0651265e
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-01 21:41:43 +0100

    Update Valid Parentheses (Easy) [Python3]

--- solutions/0020-valid-parentheses/valid-parentheses.py
+++ solutions/0020-valid-parentheses/valid-parentheses.py
@@ -1,6 +1,6 @@
 # Valid Parentheses (Easy)
 # https://leetcode.com/problems/valid-parentheses/
-# Accepted 2026-07-10 — Python3, runtime 0 ms, memory 19.4 MB
+# Accepted 2026-08-01 — Python3, runtime 0 ms, memory 19.3 MB
 class Solution:
     def isValid(self, s: str) -> bool:
         
@@ -18,24 +18,21 @@ class Solution:
 
         }
 
-
         stack=[]
 
 
         for char in s:
 
-            if char in pairOfBrackets:
+            if char=="(" or char=="{" or char=="[":
+                stack.append(char)
+            else:
                 if stack:
                     x=stack.pop()
-                elif not stack:
+                else:
                     return False
-                if x!= pairOfBrackets[char]:
+                if pairOfBrackets[char]!=x:
                     return False
 
-            else:
-                stack.append(char)
-
-        if stack: 
+        if stack:
             return False
-        else:
-            return True
+        else: return True
