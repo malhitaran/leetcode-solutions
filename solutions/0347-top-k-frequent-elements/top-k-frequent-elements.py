458386c76bfbbf4d70c50564079046e9ca6c3361
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-01 14:49:26 +0100

    Update Top K Frequent Elements (Medium) [Python3]

--- solutions/0347-top-k-frequent-elements/top-k-frequent-elements.py
+++ solutions/0347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,43 +1,25 @@
 # Top K Frequent Elements (Medium)
 # https://leetcode.com/problems/top-k-frequent-elements/
-# Accepted 2026-07-04 — Python3, runtime 10 ms, memory 24.7 MB
+# Accepted 2026-08-01 — Python3, runtime 0 ms, memory 23 MB
 from collections import Counter
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        
         '''
 
-        bucket sort
+        heap question
 
-        freq where its a list of a list
-
-
-        [[]] and each index represents the count
-
-        and at each index we have a list of numbers
-
-        and then we go backwards
+        so we get the counts, then we store as a list tuple with -count and then the item then add 1 to it and push it back onto the heap 
 
         '''
-
-
-        valueFrequency=Counter(nums)
-
-        frequency=[[] for i in range(len(nums)+1)]
         
-        res=[]
-        
-        for num, count in valueFrequency.items():
+        numFreq=Counter(nums)
+        heap=[]
+        for num, count in numFreq.items():
+            if len(heap)==k:
+                heapq.heappushpop(heap, (count, num))
+            else:
+                heapq.heappush(heap, (count, num))
 
-            frequency[count].append(num)
-
-       
-        for i in range(len(nums), 0, -1):
-           
-            for num in frequency[i]:
-
-                res.append(num)
-                if len(res)==k:
-                    return res
+        return [item[1] for item in heap]
