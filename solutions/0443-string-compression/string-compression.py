048b55658761d017f44e2d95ae0ed008d5d978d9
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-01 10:35:25 +0100

    Update String Compression (Medium) [Python3]

--- solutions/0443-string-compression/string-compression.py
+++ solutions/0443-string-compression/string-compression.py
@@ -1,38 +1,24 @@
 # String Compression (Medium)
 # https://leetcode.com/problems/string-compression/
-# Accepted 2026-08-01 — Python3, runtime 0 ms, memory 19.3 MB
+# Accepted 2026-08-01 — Python3, runtime 3 ms, memory 19.4 MB
 class Solution:
     def compress(self, chars: List[str]) -> int:
-
-        left=0
-        prev=''
+        i=0
         count=0
-        
-        for i,char in enumerate(chars):
+        left=0
+        while i<len(chars):
+            current_char=chars[i]
+            count=0
 
-            if i==0:
-                prev=char
-                count=int(count)+1
+            while i < len(chars) and current_char==chars[i]:
+                i+=1
+                count+=1
             
-            elif char==prev:
-                count=int(count)+1
-               
-            else:
-                chars[left]=prev
-                prev=char
-                left+=1
-                if count>1:
-                    count=str(count)
-                    for i in range(len(count)):
-                        chars[left]=count[i]
-                        left+=1
-                count=int(1)
-
-        chars[left] = prev
-        left += 1
-        if count > 1:
-            for digit in str(count):
-                chars[left] = digit
-                left += 1
-
+            chars[left]=current_char
+            left+=1
+            if count>1:
+                count=str(count)
+                for x in range(len(count)):
+                    chars[left]=count[x]
+                    left+=1
         return left
