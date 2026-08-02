c7bcf51a4a86c67bb61f3142657907ae6ff0ecc6
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-02 09:00:45 +0100

    Update Minimum Window Substring (Hard) [Python3]

--- solutions/0076-minimum-window-substring/minimum-window-substring.py
+++ solutions/0076-minimum-window-substring/minimum-window-substring.py
@@ -1,37 +1,45 @@
 # Minimum Window Substring (Hard)
 # https://leetcode.com/problems/minimum-window-substring/
-# Accepted 2026-06-21 — Python3, runtime 138 ms, memory 20 MB
+# Accepted 2026-08-02 — Python3, runtime 79 ms, memory 19.8 MB
 
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
-        seen=Counter(t)
-        missing=len(t)
+        '''
+
+        we can go through the string
+
+
+        '''
+
         best=float('infinity')
-        l, r =0, 0
-        sl, sr=0,0
+        l,r=0,0
+        tFreq=Counter(t)
+        missing=sum(tFreq.values())
+
+        lStore,rStore=0,0
 
         while r<len(s):
-            letter=s[r]
-            #if its in
-            print(missing)
-            if letter in seen:
-                if seen[letter]>0:
+            if s[r] in tFreq:
+                if tFreq[s[r]]>0:
                     missing-=1
-                seen[letter]-=1
-    
-            while missing ==0:
-                if (r-l+1)<best:
-                    best=min(best, r-l+1)
-                    sl=l
-                    sr=r
-                if s[l] in seen:
-                    seen[s[l]]+=1
-                    if seen[s[l]]>0:
+                tFreq[s[r]]-=1
+                r+=1
+            else:
+                r+=1
+            while missing==0:
+                if r-l+1<best:
+                    lStore=l
+                    rStore=r
+                    best=r-l+1
+               
+                if s[l] in tFreq:
+                    tFreq[s[l]]+=1
+                    if tFreq[s[l]]>0:
                         missing+=1
                 l+=1
-
-            r+=1
-    
-        return s[sl:sr + 1] if best != float('infinity') else ""
+            
+            
+                            
+        return s[lStore:rStore]
