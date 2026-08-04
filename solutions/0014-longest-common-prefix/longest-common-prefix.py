fe7f6a34b331698b15ced2e5c03ca718c2587635
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-04 12:06:59 +0100

    Update Longest Common Prefix (Easy) [Python3]

--- solutions/0014-longest-common-prefix/longest-common-prefix.py
+++ solutions/0014-longest-common-prefix/longest-common-prefix.py
@@ -1,41 +1,17 @@
 # Longest Common Prefix (Easy)
 # https://leetcode.com/problems/longest-common-prefix/
-# Accepted 2026-08-04 — Python3, runtime 0 ms, memory 19.2 MB
+# Accepted 2026-08-04 — Python3, runtime 3 ms, memory 19.3 MB
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        '''
-w       we take the max one and thats our comparison
-        then we go through each character
-        if we ever hit 0 return
+        res=""
 
-        otherwise we go through 
-
-        actually much more efficient 
-
-        we compare 0 in every o
-
-        '''
-        
-
-        #get the min
-
-        #nk
-
-        smallest=float('infinity')
-
-        for word in strs:
-            smallest=min(smallest, len(word))
-
-
-        for charNum in range(smallest):
-            ourS=set()
+        for i in range(len(strs[0])):
             for word in strs:
-                ourS.add(word[charNum])
+
+                if i==len(word) or word[i]!=strs[0][i]:
+                    return res
+
+            res+=strs[0][i]
             
-            if len(ourS)>=2:
-                
-                return strs[0][:charNum]
-        
-        
-        return strs[0][:smallest]
+        return res
