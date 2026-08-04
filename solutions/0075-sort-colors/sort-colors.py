dea082440a63a6c75b3ebd4215285074b85a9a07
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-04 18:52:32 +0100

    Update Sort Colors (Medium) [Python3]

--- solutions/0075-sort-colors/sort-colors.py
+++ solutions/0075-sort-colors/sort-colors.py
@@ -1,25 +1,52 @@
 # Sort Colors (Medium)
 # https://leetcode.com/problems/sort-colors/
-# Accepted 2026-08-04 — Python3, runtime 0 ms, memory 19.5 MB
+# Accepted 2026-08-04 — Python3, runtime 0 ms, memory 19.4 MB
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
 
 
+        [1,0,2]
+         l   r
+
+
+
+        [1, 1, 2, 2]
+        l.     r  
+
+        [0, 0, 1, 1, 2, 2]
+               l     r
+
+        switch
+
+        3 pointers
+
 
         """
     
-        colourFreq=Counter(nums)
+        l, r=0, len(nums)-1
 
-        x=0
-        for i in range(3):
+        for curr, num in enumerate(nums):
 
-            while colourFreq[i]>0:
-                
-                print(x)
-                nums[x]=i
-                x+=1
-                colourFreq[i]-=1
+            while (nums[curr]==0 and curr>l) or (nums[curr]==2 and curr<r):
+                if nums[curr]==0 and curr>l:
+                    #swap l and curr
+                    temp=nums[l]
+                    nums[l]=nums[curr]
+                    nums[curr]=temp
+                    l+=1
+                else:
+                    #swap r and curr
+                    temp=nums[r]
+                    nums[r]=nums[curr]
+                    nums[curr]=temp
+                    r-=1
 
-        return nums
+            
+
+            while l<len(nums) and nums[l]==0:
+                l+=1
+
+            while r>-1 and nums[r]==2:
+                r-=1
