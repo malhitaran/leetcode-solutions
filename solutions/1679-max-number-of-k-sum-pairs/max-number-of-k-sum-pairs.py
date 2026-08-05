05ff0cd4dc292c5834d9233ddce8f4057bf7c95e
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-05 14:32:36 +0100

    Update Max Number of K-Sum Pairs (Medium) [Python3]

--- solutions/1679-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ solutions/1679-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,25 +1,20 @@
 # Max Number of K-Sum Pairs (Medium)
 # https://leetcode.com/problems/max-number-of-k-sum-pairs/
-# Accepted 2026-05-18 — Python3, runtime 472 ms, memory 32.4 MB
-
-from collections import defaultdict
+# Accepted 2026-08-05 — Python3, runtime 473 ms, memory 32.3 MB
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         
-        '''
 
-        using dict
-        '''
-
-        collect=defaultdict(int)
+        seen=defaultdict(int)
         ops=0
-        
         for num in nums:
-            need=k-num
 
-            if collect[need]>0:
+            if k-num in seen:
                 ops+=1
-                collect[need]-=1
+                seen[k-num]-=1
+                if seen[k-num]<=0:
+                    seen.pop(k-num)
             else:
-                collect[num]+=1
+                seen[num]+=1 
+
         return ops
