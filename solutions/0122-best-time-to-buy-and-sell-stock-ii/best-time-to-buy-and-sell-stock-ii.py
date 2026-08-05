57eec1a2b1d0faaacc19730f91a03e530788108c
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-05 11:37:34 +0100

    Update Best Time to Buy and Sell Stock II (Medium) [Python3]

--- solutions/0122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,31 +1,19 @@
 # Best Time to Buy and Sell Stock II (Medium)
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
-# Accepted 2026-08-05 — Python3, runtime 0 ms, memory 20.4 MB
+# Accepted 2026-08-05 — Python3, runtime 2 ms, memory 20.4 MB
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         '''
-        [1,2,3,4,5]
+        [3,3,5,0,0,3,1,4]
 
-        l:
-        h:5
-           c
+
         '''
-
-
-        low, h=prices[0], 0
         profit=0
-        for curr in prices:
 
-            if curr<h:
-                if h>low:
-                    profit+=h-low
-                low =curr
-                h=curr
-            
-            if curr>=h:
-                h=curr
+        for i in range(1, len(prices)):
 
-        if h>low and h==curr:
-            profit+=h-low
+            if prices[i]>prices[i-1]:
+                profit+=prices[i]-prices[i-1]
+
         return profit
