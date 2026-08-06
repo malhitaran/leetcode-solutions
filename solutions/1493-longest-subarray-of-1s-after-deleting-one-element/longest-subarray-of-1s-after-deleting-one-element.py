d19f118b4d185341038e89ee88696bb4e95e2e4b
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-06 11:08:21 +0100

    Update Longest Subarray of 1's After Deleting One Element (Medium) [Python3]

--- solutions/1493-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ solutions/1493-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,20 +1,34 @@
 # Longest Subarray of 1's After Deleting One Element (Medium)
 # https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
-# Accepted 2026-03-06 — Python3, runtime 48 ms, memory 24.4 MB
+# Accepted 2026-08-06 — Python3, runtime 43 ms, memory 24.6 MB
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        zeros=0
-        l=0
-        best=0
+
+
+        '''
+
+        [1,1,0,1]
+
+
+        '''
+        
+
+        l, cnt, res=0,0,0
+        k=1
 
         for r in range(len(nums)):
-            if nums[r]==0:
-                zeros+=1
-            
-            while zeros>1:
-                if nums[l]==0:
-                    zeros-=1
-                l+=1
-            best=max(best, r-l)
 
-        return best
+            if nums[r] ==1:
+                cnt+=1
+            else:
+                k-=1
+                while k<0:
+
+                    if nums[l]==0:
+                        k+=1
+                    else:
+                        cnt-=1
+                    l+=1
+            res=max(res, cnt)
+
+        return res-1 if k==1 else res
