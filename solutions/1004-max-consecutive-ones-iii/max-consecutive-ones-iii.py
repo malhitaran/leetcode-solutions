8f53665b2adeb4752aeeb85a81514904ae95245a
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-06 10:44:12 +0100

    Update Max Consecutive Ones III (Medium) [Python3]

--- solutions/1004-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ solutions/1004-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,23 +1,40 @@
 # Max Consecutive Ones III (Medium)
 # https://leetcode.com/problems/max-consecutive-ones-iii/
-# Accepted 2026-02-09 — Python3, runtime 63 ms, memory 22.5 MB
-from typing import List
-
+# Accepted 2026-08-06 — Python3, runtime 159 ms, memory 22.4 MB
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        l = 0
-        zeros = 0
-        best = 0
 
+        '''
+        [1,1,1,0,0,0,1,1,1,1,0]
+        
+        l r 
+
+
+        while k<0:
+            l+=1
+            if l is 0 then cnt+=1
+
+        '''
+
+        l,cnt,res=0,0,0
+        
         for r in range(len(nums)):
-            if nums[r] == 0:
-                zeros += 1
 
-            while zeros > k:
-                if nums[l] == 0:
-                    zeros -= 1
-                l += 1
+            if nums[r] == 1:
+                cnt+=1
+            else:
+                k-=1
+                cnt+=1
+                while k<0:
+                    print('yes')
+                    print(nums[l])
+                    if nums[l]==0:
+                        
+                        k+=1
+                    cnt-=1
+                    l+=1
+                
+            
+            res=max(res, cnt)
 
-            best = max(best, r - l + 1)
-
-        return best
+        return res
