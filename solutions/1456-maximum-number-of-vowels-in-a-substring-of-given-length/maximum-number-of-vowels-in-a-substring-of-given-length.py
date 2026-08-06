e37829e9923b82bbffbb9a6d50c036aebae60000
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-06 10:18:07 +0100

    Update Maximum Number of Vowels in a Substring of Given Length (Medium) [Python3]

--- solutions/1456-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ solutions/1456-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,22 +1,47 @@
 # Maximum Number of Vowels in a Substring of Given Length (Medium)
 # https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
-# Accepted 2026-02-08 — Python3, runtime 63 ms, memory 19.7 MB
+# Accepted 2026-08-06 — Python3, runtime 126 ms, memory 20 MB
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
 
-        vowels=["a","e","i","o","u"]
-        current=0
+        '''
+
+        sliding window
+
+        we will have a count, then we know if the next letter is a vowel we will have to recalculate whether its the best, if the last element was a vowel we minus 1 from the count. 
+
+
+        first process the first k, 
+
+
+        '''
+        vowels={'A','a','E','e','I','i','O','o','U','u'}
+
         
+
+        #process the first k
+
+        out=0
         for i in range(k):
             if s[i] in vowels:
-                current+=1
-        max=current
-        for i in range(k, len(s)):
-            if s[i-k] in vowels:
-                current-=1
-            if s[i] in vowels:
-                current+=1
-            if current>max:
-                max=current
+                out+=1
+        best=out
         
-        return max
+        
+        for i in range(k, len(s)):
+            
+            
+
+            if s[i] in vowels:
+                out+=1
+
+            if s[i-k] in vowels:
+                out-=1
+            
+            print(out)
+            if out>best:
+                best=out
+        
+        
+
+        return best
