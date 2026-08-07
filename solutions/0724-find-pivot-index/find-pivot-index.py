0751419740eeb686deccf4e742ba84525345ecfd
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-07 13:30:02 +0100

    Update Find Pivot Index (Easy) [Python3]

--- solutions/0724-find-pivot-index/find-pivot-index.py
+++ solutions/0724-find-pivot-index/find-pivot-index.py
@@ -1,17 +1,40 @@
 # Find Pivot Index (Easy)
 # https://leetcode.com/problems/find-pivot-index/
-# Accepted 2026-03-06 — Python3, runtime 7 ms, memory 20.4 MB
-from typing import List
-
+# Accepted 2026-08-07 — Python3, runtime 7 ms, memory 20.3 MB
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        total = sum(nums)
-        left_sum = 0
+        
+        '''
+        sum left
 
-        for i in range(len(nums)):
-            if left_sum == total - left_sum - nums[i]:
+        sum right
+
+        [1,7,3,6,5,6]
+
+        28-6 22 11
+
+
+
+        [-1, -1, -1, -1, -1]
+
+        -5
+
+        -1 -2 -4/2 = -2
+
+
+        [2,1,-1]
+        2
+        
+        0
+        '''
+
+
+        totalSum=sum(nums)
+        prefixSum=0
+
+        for i, num in enumerate(nums):
+            if (totalSum-num)/2==prefixSum:
                 return i
-            
-            left_sum += nums[i]
-
+            else:
+                prefixSum+=num
         return -1
