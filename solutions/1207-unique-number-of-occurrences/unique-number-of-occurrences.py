755454c4ad2b600ce37a8a4fd6787c91bf2f1d46
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-07 14:07:38 +0100

    Update Unique Number of Occurrences (Easy) [Python3]

--- solutions/1207-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ solutions/1207-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,18 +1,18 @@
 # Unique Number of Occurrences (Easy)
 # https://leetcode.com/problems/unique-number-of-occurrences/
-# Accepted 2026-03-07 — Python3, runtime 0 ms, memory 19.2 MB
+# Accepted 2026-08-07 — Python3, runtime 0 ms, memory 19.4 MB
+from collections import Counter
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        
-        counts={}
 
-        for num in arr:
-            if num not in counts:
-                counts[num]=1
+
+        numFreq=Counter(arr)
+        ourCount=set()
+    
+        for freq in numFreq.values():
+            if freq in ourCount:
+                return False
             else:
-                counts[num]+=1
-        
-        comp=list(counts.values())
-        setComp=set(comp)
+                ourCount.add(freq)
 
-        return len(setComp) == len(comp)
+        return True
