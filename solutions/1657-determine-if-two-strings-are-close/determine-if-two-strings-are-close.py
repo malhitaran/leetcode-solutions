b4138d829d4ba2b5f1c5d741751e94d5bbf6464d
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-07 14:28:48 +0100

    Update Determine if Two Strings Are Close (Medium) [Python3]

--- solutions/1657-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ solutions/1657-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,26 +1,26 @@
 # Determine if Two Strings Are Close (Medium)
 # https://leetcode.com/problems/determine-if-two-strings-are-close/
-# Accepted 2026-03-07 — Python3, runtime 95 ms, memory 20.4 MB
+# Accepted 2026-08-07 — Python3, runtime 75 ms, memory 20.4 MB
 from collections import Counter
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        
+        if len(word1)!=len(word2):
+            return False
 
-        '''
-        case 1:compare lengths of word 1 and word2 if there equal and the sets of both are equal(contain the same stuff and no difference) that means both sets have the same letters(by sets) and the same amount of words(by len)
+        if set(word1) != set(word2):
+            return False
+        
+        word1Freq=Counter(word1)
+        word2Freq=Counter(word2)
 
-        case 2:get the occurences if the occurence is the same and the set of words are the same then its fine
+        word1Freq=list(word1Freq.values())
+        word2Freq=list(word2Freq.values())
 
+        word1Freq.sort()
+        word2Freq.sort()
 
-        aaabc   
-        iiias
-        aabbbbcc
-        bca
-        cccab set occur
-        aaacb
-
-        '''
-
-        count1=Counter(word1)
-        count2=Counter(word2)
-
-        return set(word1)==set(word2) and Counter(count1.values())==Counter(count2.values())
+        if word1Freq != word2Freq:
+            print('here')
+            return False
+        return True
