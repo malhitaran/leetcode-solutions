5b56d158abc19556f15ded20c71a64eea400dced
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-07 13:39:34 +0100

    Update Find the Difference of Two Arrays (Easy) [Python3]

--- solutions/2215-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ solutions/2215-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -1,10 +1,19 @@
 # Find the Difference of Two Arrays (Easy)
 # https://leetcode.com/problems/find-the-difference-of-two-arrays/
-# Accepted 2026-03-07 — Python3, runtime 11 ms, memory 19.5 MB
+# Accepted 2026-08-07 — Python3, runtime 13 ms, memory 19.5 MB
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        
-        set1=set(nums1)
-        set2=set(nums2)
 
-        return [list(set1-set2), list(set2-set1)]
+        res1=set(nums1)
+        res2=set(nums2)
+        
+
+        for num2 in nums2:
+            if num2 in res1:
+                res1.remove(num2)
+
+        for num1 in nums1:
+            if num1 in res2:
+                res2.remove(num1)
+
+        return [list(res1), list(res2)]
