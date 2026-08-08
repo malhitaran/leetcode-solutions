f84fb6f2c765c5f862780ddcce13c5a64afe5066
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-08 11:31:23 +0100

    Update Removing Stars From a String (Medium) [Python3]

--- solutions/2390-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ solutions/2390-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -1,12 +1,16 @@
 # Removing Stars From a String (Medium)
 # https://leetcode.com/problems/removing-stars-from-a-string/
-# Accepted 2026-03-08 — Python3, runtime 83 ms, memory 20.6 MB
+# Accepted 2026-08-08 — Python3, runtime 85 ms, memory 20.4 MB
 class Solution:
     def removeStars(self, s: str) -> str:
-        stack = []
+
+
+        stack=[]
+
         for char in s:
-            if char == '*':
-                stack.pop()  # remove previous char
-            else:
+
+            if char !="*":
                 stack.append(char)
+            else:
+                stack.pop()
         return ''.join(stack)
