3507fa56cd5813bf71b25ac3822b97f44fce61d7
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-08 13:58:39 +0100

    Update Decode String (Medium) [Python3]

--- solutions/0394-decode-string/decode-string.py
+++ solutions/0394-decode-string/decode-string.py
@@ -1,32 +1,42 @@
 # Decode String (Medium)
 # https://leetcode.com/problems/decode-string/
-# Accepted 2026-03-09 — Python3, runtime 0 ms, memory 19.3 MB
+# Accepted 2026-08-08 — Python3, runtime 0 ms, memory 19.5 MB
 class Solution:
     def decodeString(self, s: str) -> str:
+        
+        '''
+        3[a]2[bc]
 
+        arr=[]
+        stack=[]
 
-        num_stack = []
-        str_stack = []
-        curr_num = 0
-        curr_str = ""
+        until theres a closing bracket
+
+        '''
+        res=[]
+        stack=[]
+        temp=[]
 
         for char in s:
-            if char.isdigit():
-                # build the number, e.g., "12[a]"
-                curr_num = curr_num * 10 + int(char)
-            elif char == '[':
-                # push current number and string onto stacks
-                num_stack.append(curr_num)
-                str_stack.append(curr_str)
-                curr_num = 0
-                curr_str = ""
-            elif char == ']':
-                # pop number and previous string, repeat current string
-                repeat_times = num_stack.pop()
-                prev_str = str_stack.pop()
-                curr_str = prev_str + curr_str * repeat_times
+            temp=[]
+            if char!=']':
+                stack.append(char)
             else:
-                # regular character, append
-                curr_str += char
+                x=stack.pop()
+                while stack and not x.isdigit():
+                    if x!='[':
+                        temp.append(x)
+                    x=stack.pop()
+                while stack and stack[-1].isdigit():
+                    x= stack.pop()+x
 
-        return curr_str
+                temp.reverse()
+                for c in int(x)*temp:
+                    stack.append(c)
+
+            
+                
+            
+            print(stack)
+           
+        return ''.join(stack)
