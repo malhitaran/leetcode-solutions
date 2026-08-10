c001dd5d6f6ab99e893521f9514b7fc4f45269df
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-10 16:04:13 +0100

    Update Delete the Middle Node of a Linked List (Medium) [Python3]

--- solutions/2095-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ solutions/2095-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -1,11 +1,6 @@
 # Delete the Middle Node of a Linked List (Medium)
 # https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
-# Accepted 2026-03-10 — Python3, runtime 96 ms, memory 62.4 MB
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# Accepted 2026-08-10 — Python3, runtime 67 ms, memory 63 MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -14,18 +9,24 @@
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        if not head.next:
-            return None
+        dummy=ListNode()
+        dummy.next=head
+        count=0
+        
+        fast,slow=head,head
+        prev=head
 
-        slow = head
-        fast = head
-        prev = None
 
         while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
+            prev=slow
+            slow=slow.next
 
-        prev.next = slow.next
+            fast=fast.next.next
+
+
+        if prev and prev.next:
+            prev.next=prev.next.next
+        else:
+            return dummy.next.next
 
         return head
