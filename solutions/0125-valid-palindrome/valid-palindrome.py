ac46ab5545bf4e02789240fe47b37024088eaafb
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-13 10:59:58 +0100

    Update Valid Palindrome (Easy) [Python3]

--- solutions/0125-valid-palindrome/valid-palindrome.py
+++ solutions/0125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,13 @@
 # Valid Palindrome (Easy)
 # https://leetcode.com/problems/valid-palindrome/
-# Accepted 2026-08-13 — Python3, runtime 7 ms, memory 20.7 MB
+# Accepted 2026-08-13 — Python3, runtime 4 ms, memory 20.3 MB
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        cleaned = re.sub(r"[^a-zA-Z0-9]", "", s)
-
-        cleaned=cleaned.lower()
-        print(cleaned)
-        print(cleaned[::-1])
-        return cleaned==cleaned[::-1]
+        temp=[]
+        for ch in s:
+            if ch.isalnum():
+                temp.append(ch)
+        s=''.join(temp)
+        s=s.lower()
+        return s==s[::-1]
