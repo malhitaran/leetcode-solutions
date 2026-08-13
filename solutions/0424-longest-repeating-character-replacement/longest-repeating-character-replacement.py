8063958da85f863c8ec512e5c58754a99c09d66d
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-13 14:38:39 +0100

    Update Longest Repeating Character Replacement (Medium) [Python3]

--- solutions/0424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ solutions/0424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,28 +1,40 @@
 # Longest Repeating Character Replacement (Medium)
 # https://leetcode.com/problems/longest-repeating-character-replacement/
-# Accepted 2026-06-27 — Python3, runtime 144 ms, memory 19.7 MB
+# Accepted 2026-08-13 — Python3, runtime 276 ms, memory 29.2 MB
 from collections import defaultdict
+import heapq
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
         '''
+        maybe we can see the most frequent character at any point 
+        using a heap then doing a sliding window
 
-        "AABABBA"
-
-
+        so basically we keep a dictionary then heapify it get the most common and and then keep a sum and then minus from teh sum
         '''
-        count={}
+        
+        ourDict=defaultdict(int)
+        curS=0
         l=0
-
-        res=0
-
+        heap=[]
+        best=0
         for r in range(len(s)):
+            curS+=1
+            ourDict[s[r]]+=1
+            heapq.heappush(heap, (-ourDict[s[r]], s[r]))
 
-            count[s[r]]=1+count.get(s[r], 0)
-
-            while (r-l+1)-max(count.values())>k:
-                count[s[l]]-=1
-                l+=1
             
-            res=max(res, r-l+1)
-        return res
+            while -heap[0][0] != ourDict[heap[0][1]]:
+                heapq.heappop(heap)
+
+            while curS-(-heap[0][0])>k:
+                ourDict[s[l]]-=1
+                curS-=1
+                l+=1
+                
+                while -heap[0][0] != ourDict[heap[0][1]]:
+                    heapq.heappop(heap)
+
+            
+            best=max(best, r-l+1)
+        return best
