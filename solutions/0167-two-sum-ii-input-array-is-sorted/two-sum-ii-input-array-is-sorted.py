e71507118f77389ee7787ca6654c5f00b775f8b1
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-13 11:12:59 +0100

    Update Two Sum II - Input Array Is Sorted (Medium) [Python3]

--- solutions/0167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ solutions/0167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,20 +1,29 @@
 # Two Sum II - Input Array Is Sorted (Medium)
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-# Accepted 2026-06-20 — Python3, runtime 7 ms, memory 20.4 MB
+# Accepted 2026-08-13 — Python3, runtime 5 ms, memory 20.5 MB
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
 
-        start=0
-        back=len(numbers)-1
+        '''
+        constant extra space is tricky
 
+        i identified it was two pointers if were using no extra space
 
-        while start<back:
-            x=numbers[start]+numbers[back]
+        main idea we have a target
 
-            if x==target:
-                return [start+1,back+1]
-            if x>target:
-                back-=1
+        if our sum is less than then we decrement 
+        vice versa
+
+        '''
+
+        l,r=0,len(numbers)-1
+
+        while l<r:
+            
+            ourS=numbers[l]+numbers[r]
+            if ourS==target:
+                return [l+1, r+1]
+            elif ourS<target:
+                l+=1
             else:
-                start+=1
+                r-=1
