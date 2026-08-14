dfa959279838ffbb3307a8910426e387e7022aaf
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-14 15:26:55 +0100

    Update Koko Eating Bananas (Medium) [Python3]

--- solutions/0875-koko-eating-bananas/koko-eating-bananas.py
+++ solutions/0875-koko-eating-bananas/koko-eating-bananas.py
@@ -1,20 +1,61 @@
 # Koko Eating Bananas (Medium)
 # https://leetcode.com/problems/koko-eating-bananas/
-# Accepted 2026-06-27 — Python3, runtime 151 ms, memory 20.7 MB
+# Accepted 2026-08-14 — Python3, runtime 157 ms, memory 20.7 MB
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        hi=max(piles)
-        lo=1
-        best=0
         
-        while lo<=hi:
-            mid=((hi-lo)//2)+lo
-            it=0
-            for num in piles:
-                it+=ceil(num/mid)
-            if it<=h:
-                best = mid
-                hi = mid - 1
+        '''
+        okay so we have a array 
+
+        [30,11,23,4,20]
+        these are piles of bananas
+
+        the first pile has 30 bananas
+
+        the guards will come back in h hours
+
+        how many bananas does koko need to eat the minimum
+
+        need to find the minimum
+
+
+        range of bananas 0-max
+
+        [3,6,7,11]
+
+        [0,1,2,3,4,5,6,7,8,9,10,11]
+
+
+        print(start, 'start')
+        print(end, 'end')
+        print(mid, 'mid')
+        print(temp, 'temp')
+        print(best, 'best')
+        '''
+        
+        
+        '''
+        for i in range(1, max(piles)+1):
+            r.append(i)
+
+        '''
+
+        start,end,best=1, max(piles),0
+        i=0
+        while start<=end:
+            mid=(end+start)//2
+            temp=0
+            
+            for pile in piles:
+                temp+=ceil((pile )/(mid))
+
+            if temp<=h and temp>=best:
+                best=temp
+                i=mid
+
+            if temp>h:
+                start=mid+1
             else:
-                lo = mid + 1
-        return best
+                end=mid-1
+           
+        return i
