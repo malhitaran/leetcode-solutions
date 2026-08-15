b8fe78a63bff8b8f1380e178e43b8cfd84f8c640
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-15 18:47:20 +0100

    Update Search a 2D Matrix (Medium) [Python3]

--- solutions/0074-search-a-2d-matrix/search-a-2d-matrix.py
+++ solutions/0074-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,41 +1,25 @@
 # Search a 2D Matrix (Medium)
 # https://leetcode.com/problems/search-a-2d-matrix/
-# Accepted 2026-06-27 — Python3, runtime 0 ms, memory 19.7 MB
+# Accepted 2026-08-15 — Python3, runtime 0 ms, memory 19.7 MB
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        '''
-        how do we get n and m when we got index 5
-     
-
- 
-
-        n=nums//m
-        m=nums mod m -1
-
-
-
-        '''
-
-
-        end=(len(matrix[0])*len(matrix))-1
-        start=0
-        m=len(matrix[0])
+       
+        amount=len(matrix)*len(matrix[0])
+        start,end=0,amount-1
+        
 
         while start<=end:
-            print(end)
-            midpoint=((end-start)//2)+start
-        
-            x=midpoint//m
-            y=(midpoint%m)
-            if y<0:
-                y=0
-
-           
-            if matrix[x][y]==target:
+            mid=(start+end)//2
+            print(mid)
+            r=mid//len(matrix[0])
+            c=mid%len(matrix[0])
+            print(r,c)
+            if matrix[r][c]==target:
                 return True
-            elif matrix[x][y]>target:
-                end=midpoint-1
+        
+            if matrix[r][c]>target:
+                end=mid-1
             else:
-                start=midpoint+1
-                print(start)
+                start=mid+1
+
         return False
