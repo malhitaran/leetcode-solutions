d9405e43042dc8198a488cf3cacb0af04fa4ae9f
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-15 11:05:44 +0100

    Update Add Two Numbers (Medium) [Python3]

--- solutions/0002-add-two-numbers/add-two-numbers.py
+++ solutions/0002-add-two-numbers/add-two-numbers.py
@@ -1,6 +1,6 @@
 # Add Two Numbers (Medium)
 # https://leetcode.com/problems/add-two-numbers/
-# Accepted 2026-06-28 — Python3, runtime 4 ms, memory 19.2 MB
+# Accepted 2026-08-15 — Python3, runtime 89 ms, memory 19.4 MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,33 +9,28 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-
-        '''
-        we go n times
-
-        we add the values, if its less than 10 perfect we store it as a new node with that value
-
-        if its greater than 10 we carry 
-
-        [9,9,9,9,9,9,9], 
-        [9,9,9,9]
-        8, 9, 9 9, 0, 0, 0, 1
-        we need to do it from the bigger list
-        '''
-        dummy=ListNode()
-        curr=dummy
+        res=ListNode()
+        tail=res
         carry=0
-        while l1 or l2 or carry:
-            v1=l1.val if l1 else 0
-            v2=l2.val if l2 else 0
-
-            currSum=v1+v2+carry
-            carry=currSum //10
-            value=currSum%10
-            curr.next=ListNode(value)
-
-            curr=curr.next
+        while l1 or l2:
+            if l1==None:
+                total=l2.val+carry
+            elif l2==None:
+                total=l1.val+carry
+            else:
+                total=l1.val+l2.val+carry
+            
+            print(total)
+            tail.next=ListNode(total%10, None)
+            print(res)
+            if total>9:
+                carry=1
+            else:
+                carry=0
+            tail=tail.next
             l1=l1.next if l1 else None
             l2=l2.next if l2 else None
-
-        return dummy.next
+       
+        if carry==1:
+            tail.next=ListNode(1, None)
+        return res.next
