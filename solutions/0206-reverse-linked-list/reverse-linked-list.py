fc0af1c30726f8c94148eb0ef1101f2787348778
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-15 20:27:27 +0100

    Update Reverse Linked List (Easy) [Python3]

--- solutions/0206-reverse-linked-list/reverse-linked-list.py
+++ solutions/0206-reverse-linked-list/reverse-linked-list.py
@@ -1,6 +1,6 @@
 # Reverse Linked List (Easy)
 # https://leetcode.com/problems/reverse-linked-list/
-# Accepted 2026-06-28 — Python3, runtime 0 ms, memory 20.2 MB
+# Accepted 2026-08-15 — Python3, runtime 0 ms, memory 20.3 MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,24 +9,12 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        '''
-        [1,2,3,4,5]
+        curr,prev=head,None
 
-        we go through one item at a time, save the next node, set it to the previous node
-        '''
-
-        prev=None
-        curr=head
-        while curr!=None:
-       
-            temp=curr.next
-            
+        while curr:
+            tmp=curr.next
             curr.next=prev
-            
             prev=curr
-            
-            curr=temp
-           
-
+            curr=tmp
         
         return prev
