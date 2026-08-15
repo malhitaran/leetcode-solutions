9e3b1a07f78030048b1ba0b5c5d30d95cf937636
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-15 11:43:08 +0100

    Update Invert Binary Tree (Easy) [Python3]

--- solutions/0226-invert-binary-tree/invert-binary-tree.py
+++ solutions/0226-invert-binary-tree/invert-binary-tree.py
@@ -1,6 +1,6 @@
 # Invert Binary Tree (Easy)
 # https://leetcode.com/problems/invert-binary-tree/
-# Accepted 2026-06-30 — Python3, runtime 0 ms, memory 19.2 MB
+# Accepted 2026-08-15 — Python3, runtime 0 ms, memory 19.4 MB
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,16 +8,24 @@
 #         self.left = left
 #         self.right = right
 
+from collections import deque
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
 
+        '''
+
+        i think that we add the right-left children to the queue and its a bfs
+        '''
+
         queue=deque([root])
-        
+
         while queue:
             x=queue.popleft()
-            if x!=None:
-                x.left, x.right=x.right, x.left
-        
-                queue.append(x.left)
+
+            if x:
                 queue.append(x.right)
+                queue.append(x.left)
+            
+                x.left,x.right=x.right,x.left
+        
         return root
