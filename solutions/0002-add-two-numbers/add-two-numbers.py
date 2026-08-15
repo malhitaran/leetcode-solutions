c5d04bd2f4c607730e364a17593018324aefbfe9
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-15 11:14:31 +0100

    Update Add Two Numbers (Medium) [Python3]

--- solutions/0002-add-two-numbers/add-two-numbers.py
+++ solutions/0002-add-two-numbers/add-two-numbers.py
@@ -1,6 +1,6 @@
 # Add Two Numbers (Medium)
 # https://leetcode.com/problems/add-two-numbers/
-# Accepted 2026-08-15 — Python3, runtime 89 ms, memory 19.4 MB
+# Accepted 2026-08-15 — Python3, runtime 2 ms, memory 19.3 MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -12,25 +12,14 @@ class Solution:
         res=ListNode()
         tail=res
         carry=0
-        while l1 or l2:
-            if l1==None:
-                total=l2.val+carry
-            elif l2==None:
-                total=l1.val+carry
-            else:
-                total=l1.val+l2.val+carry
-            
-            print(total)
-            tail.next=ListNode(total%10, None)
-            print(res)
-            if total>9:
-                carry=1
-            else:
-                carry=0
+        while l1 or l2 or carry:
+            val1=l1.val if l1 else 0
+            val2=l2.val if l2 else 0
+            total=val1+val2+carry
+            carry=total//10
+            tail.next=ListNode(total%10)
             tail=tail.next
             l1=l1.next if l1 else None
             l2=l2.next if l2 else None
        
-        if carry==1:
-            tail.next=ListNode(1, None)
         return res.next
