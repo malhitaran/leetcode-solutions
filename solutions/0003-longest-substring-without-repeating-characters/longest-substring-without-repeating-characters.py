6f99234ca0d6846d3739c9b9a2b6dbc69c682fbd
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-15 12:46:41 +0100

    Update Longest Substring Without Repeating Characters (Medium) [Python3]

--- solutions/0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ solutions/0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,25 +1,13 @@
 # Longest Substring Without Repeating Characters (Medium)
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
-# Accepted 2026-07-03 — Python3, runtime 19 ms, memory 19.3 MB
-
-from collections import defaultdict
+# Accepted 2026-08-15 — Python3, runtime 271 ms, memory 19.9 MB
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        l,best=0,0
 
+        seen=collections.defaultdict(int)
 
-        '''
-
-
-        '''
-        
-
-        seen=defaultdict(int)
-
-        l=0
-        r=0
-        best=0
-
-        while r<len(s):
+        for r in range(len(s)):
 
             seen[s[r]]+=1
 
@@ -28,7 +16,5 @@ class Solution:
                 l+=1
             
             best=max(best, r-l+1)
-            r+=1
 
-        
         return best
