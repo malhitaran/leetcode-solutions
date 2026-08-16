d0557f436fadda58362a207f4c31b540106a5f93
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-16 12:16:19 +0100

    Update Sort Colors (Medium) [Python3]

--- solutions/0075-sort-colors/sort-colors.py
+++ solutions/0075-sort-colors/sort-colors.py
@@ -1,52 +1,37 @@
 # Sort Colors (Medium)
 # https://leetcode.com/problems/sort-colors/
-# Accepted 2026-08-04 — Python3, runtime 0 ms, memory 19.4 MB
+# Accepted 2026-08-16 — Python3, runtime 0 ms, memory 19.2 MB
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-
-
-        [1,0,2]
-         l   r
-
-
-
-        [1, 1, 2, 2]
-        l.     r  
-
-        [0, 0, 1, 1, 2, 2]
-               l     r
-
-        switch
-
-        3 pointers
-
-
         """
-    
-        l, r=0, len(nums)-1
 
-        for curr, num in enumerate(nums):
+        '''
+        [2,0,2,1,1,0]
+        [0,0,1,1,2,2]
 
-            while (nums[curr]==0 and curr>l) or (nums[curr]==2 and curr<r):
-                if nums[curr]==0 and curr>l:
-                    #swap l and curr
-                    temp=nums[l]
-                    nums[l]=nums[curr]
-                    nums[curr]=temp
-                    l+=1
-                else:
-                    #swap r and curr
-                    temp=nums[r]
-                    nums[r]=nums[curr]
-                    nums[curr]=temp
-                    r-=1
+        we may have to do 2 swaps because a swap can mean we swap a 0 and a 2 and if we move on before processing the next 2 or 0 then we can skip that case
+        '''
 
-            
+        l,index, r=0,0,len(nums)-1
 
+        while index<len(nums):
             while l<len(nums) and nums[l]==0:
                 l+=1
-
             while r>-1 and nums[r]==2:
                 r-=1
+            while (nums[index]==2 and r>index) or (nums[index]==0 and l<index):
+                if nums[index]==0:
+                    #swap with left
+                    tmp=nums[index]
+                    nums[index]=nums[l]
+                    nums[l]=tmp
+                    l+=1
+                else:
+                    tmp=nums[index]
+                    nums[index]=nums[r]
+                    nums[r]=tmp
+                    r-=1
+    
+            index+=1
