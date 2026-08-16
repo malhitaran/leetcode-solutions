2f0af56680af5bafaac82df3749446d81ac2e26f
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-16 11:03:04 +0100

    Update Longest Consecutive Sequence (Medium) [Python3]

--- solutions/0128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ solutions/0128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,24 +1,28 @@
 # Longest Consecutive Sequence (Medium)
 # https://leetcode.com/problems/longest-consecutive-sequence/
-# Accepted 2026-08-01 — Python3, runtime 49 ms, memory 36.7 MB
+# Accepted 2026-08-16 — Python3, runtime 70 ms, memory 36.7 MB
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
         '''
-        we know that they all start from a point
+        if there is no n-1 in the set then we know its a start, we can increment the count and then look for the next item 
 
-        - the big key. its at the beginning if n-1 doesnt exist in the set!!!!
+        
+
+   
         '''
 
-        ourS=set(nums)
+        track=set(nums)
         best=0
-        curr=1
-        for num in ourS:
-            curr=1
-           
-            if num-1 not in ourS:
-                while num+1 in ourS:
-                    curr+=1
-                    num=num+1
-                best=max(best,curr)
+        for num in nums:
+            count=0
+            if num-1 not in track:
+                while num in track:
+                    track.remove(num)
+                    num+=1
+                    count+=1
+                best=max(best, count)
+            else:
+                continue
+
         return best
