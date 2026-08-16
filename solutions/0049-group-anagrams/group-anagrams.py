daf6fa35e5b4026fed8cb781e604980ff3b16bcf
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-16 12:49:50 +0100

    Update Group Anagrams (Medium) [Python3]

--- solutions/0049-group-anagrams/group-anagrams.py
+++ solutions/0049-group-anagrams/group-anagrams.py
@@ -1,20 +1,23 @@
 # Group Anagrams (Medium)
 # https://leetcode.com/problems/group-anagrams/
-# Accepted 2026-07-03 — Python3, runtime 15 ms, memory 24 MB
-
+# Accepted 2026-08-16 — Python3, runtime 15 ms, memory 23.8 MB
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
 
+        '''
+        store these in a frequency bucket
 
-        countList=defaultdict(list)#keys will be a list[a-z]:frequency
+        buck -> list
+        '''
 
-        for i in range(len(strs)):
+        wordBucket=defaultdict(list)
 
+        for word in strs:
             count=[0]*26
+            for ch in word:
+                count[ord(ch)-ord('a')]+=1
+            wordBucket[tuple(count)].append(word)
 
-            for letter in strs[i]:
-                count[ord(letter)-ord('a')]+=1
-
-            countList[tuple(count)].append(strs[i])
-
-        return list(countList.values())
+        return list(wordBucket.values())
