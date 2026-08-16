634eb3281dc043cf7093d65a737a5ce79649ecef
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-16 14:08:56 +0100

    Update Top K Frequent Elements (Medium) [Python3]

--- solutions/0347-top-k-frequent-elements/top-k-frequent-elements.py
+++ solutions/0347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,40 +1,24 @@
 # Top K Frequent Elements (Medium)
 # https://leetcode.com/problems/top-k-frequent-elements/
-# Accepted 2026-08-01 — Python3, runtime 11 ms, memory 24.8 MB
+# Accepted 2026-08-16 — Python3, runtime 3 ms, memory 23 MB
 from collections import Counter
-
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
+        '''
+        going to do it the heap way first
         '''
 
-        so we know we can create buckets 
-        in these buckets we can store the numbers
-
-        e.g. if we had 10 numbers we know a number cannot occur more than 10 times. 
-
-        then we do a search backwards
-
-
-        creating the bucket is easy
-
-        '''
+        
+        numFreq=Counter(nums)
+        heap=[]
         res=[]
-        numBucket=[[] for x in range(len(nums)+1)]
-        freqNum=Counter(nums)
+        for num, freq in numFreq.items():
+            heap.append((-freq,num))
 
-        for num, freq in freqNum.items():
-            
-            numBucket[freq].append(num)
+        heapq.heapify(heap)
+        print(heap)
 
-        for ourBucket in reversed(numBucket):
-
-            if k<=0:
-                break
-            else:
-                for item in reversed(ourBucket):
-                    res.append(item)
-                    k-=1
-                    if k<=0:
-                        break
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
         return res
