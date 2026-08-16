38608853e2b42f64cc96f048c4efaafad5fb0fde
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-16 12:23:20 +0100

    Update Sort Colors (Medium) [Python3]

--- solutions/0075-sort-colors/sort-colors.py
+++ solutions/0075-sort-colors/sort-colors.py
@@ -1,6 +1,6 @@
 # Sort Colors (Medium)
 # https://leetcode.com/problems/sort-colors/
-# Accepted 2026-08-16 — Python3, runtime 0 ms, memory 19.2 MB
+# Accepted 2026-08-16 — Python3, runtime 0 ms, memory 19.1 MB
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
@@ -16,22 +16,13 @@ class Solution:
 
         l,index, r=0,0,len(nums)-1
 
-        while index<len(nums):
-            while l<len(nums) and nums[l]==0:
+        while index<=r:
+            if nums[index]==0:
+                nums[l], nums[index]=nums[index], nums[l]
+                index+=1
                 l+=1
-            while r>-1 and nums[r]==2:
+            elif nums[index]==2:
+                nums[r], nums[index]=nums[index], nums[r]
                 r-=1
-            while (nums[index]==2 and r>index) or (nums[index]==0 and l<index):
-                if nums[index]==0:
-                    #swap with left
-                    tmp=nums[index]
-                    nums[index]=nums[l]
-                    nums[l]=tmp
-                    l+=1
-                else:
-                    tmp=nums[index]
-                    nums[index]=nums[r]
-                    nums[r]=tmp
-                    r-=1
-    
-            index+=1
+            else:
+                index+=1
