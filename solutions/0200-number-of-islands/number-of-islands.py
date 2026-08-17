8f2069f5255e6445e2699f5cd718ad990fccfe40
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-17 08:32:15 +0100

    Update Number of Islands (Medium) [Python3]

--- solutions/0200-number-of-islands/number-of-islands.py
+++ solutions/0200-number-of-islands/number-of-islands.py
@@ -1,36 +1,39 @@
 # Number of Islands (Medium)
 # https://leetcode.com/problems/number-of-islands/
-# Accepted 2026-07-04 — Python3, runtime 295 ms, memory 26.6 MB
+# Accepted 2026-08-17 — Python3, runtime 389 ms, memory 27.2 MB
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
+        '''
 
-        rows, cols = len(grid), len(grid[0])
 
-        visit=set()
-                
+        '''
+        
+        rows,cols=len(grid), len(grid[0])
+        seen=set()
         islands=0
+        def bfs(r,c):
+            queue=deque([(r,c)])
+            directions=[[1,0],[0,1],[-1,0],[0,-1]]
+            while queue:
+                print(queue)
+                r,c=queue.popleft()
+                for rp, cp in directions:
+                    if (r+rp in range(rows)) and (c+cp in range(cols)) and ((r+rp,c+cp) not in seen) and grid[r+rp][c+cp]=="1":
+                        seen.add((r+rp,c+cp))
+                        queue.append((r+rp,c+cp))
+                        
 
 
-        def bfs(r, c):
-            visit.add((r, c))
-            q=collections.deque()
-            q.append((r,c))
-            directions=[[-1, 0],[1, 0],[0, 1],[0, -1]]
-            while q:
-                r,c=q.popleft()
-                for dr, cr in directions:
-                    if r+dr in range(rows) and c+cr in range(cols) and (r+dr, c+cr) not in visit and grid[r+dr][c+cr]=='1':
-                        q.append((r+dr, c+cr))
-                        visit.add((r+dr, c+cr))
-
-
-
-        for r in range(rows):
-            for c in range(cols):
-
-                if grid[r][c]=='1' and (r, c) not in visit:
-                    islands+=1
-                    bfs(r,c)
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]=="0":
+                    continue
+                if (row, col) in seen:
+                    continue
+                islands+=1
+                seen.add((row,col))
+                bfs(row,col)
 
         return islands
