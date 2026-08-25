6a4e55347f34ed18dc5ab7efc4fcffb2fa958a4a
Taranm1 <MalhiTaran182005@gmail.com>
2026-08-25 21:50:17 +0100

    Update Two Sum (Easy) [Python3]

--- solutions/0001-two-sum/two-sum.py
+++ solutions/0001-two-sum/two-sum.py
@@ -1,6 +1,6 @@
 # Two Sum (Easy)
 # https://leetcode.com/problems/two-sum/
-# Accepted 2026-08-09 — Python3, runtime 3 ms, memory 20.4 MB
+# Accepted 2026-08-25 — Python3, runtime 0 ms, memory 20.4 MB
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
