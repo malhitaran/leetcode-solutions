78345c42bb5f84ab03e98d9f485f48adc861a3b7
Taranm1 <MalhiTaran182005@gmail.com>
2026-09-05 16:53:35 +0100

    Update Longest Consecutive Sequence (Medium) [Python3]

--- solutions/0128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ solutions/0128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,15 +1,19 @@
 # Longest Consecutive Sequence (Medium)
 # https://leetcode.com/problems/longest-consecutive-sequence/
-# Accepted 2026-08-16 — Python3, runtime 35 ms, memory 36.5 MB
+# Accepted 2026-09-05 — Python3, runtime 59 ms, memory 36.6 MB
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-
-        track=set(nums)
-        best=0
-        for num in track:
-            if num-1 not in track:
-                count=num
-                while count in track:
-                    count+=1
-                best=max(best, count-num)
+        seen=set(nums)
+        best,i=0,0
+        while seen:
+            x=nums[i]
+            if x-1 in seen:
+                i+=1
+                continue
+            else:
+                while x in seen:
+                    seen.remove(x)
+                    x+=1
+                best=max(best, x-nums[i])
+                i+=1
         return best
